971bb4241394b63d4c2055cd0f7a500fa5f2b744
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-08 11:37:06 +0200

    tp2 : Bieres

--- TP2_Bieres_CHOMIENNE/src/tp2_bieres_chomienne/BouteilleBiere.java
+++ TP2_Bieres_CHOMIENNE/src/tp2_bieres_chomienne/BouteilleBiere.java
@@ -9,15 +9,45 @@ package tp2_bieres_chomienne;
  * @author lunac
  */
 public class BouteilleBiere {
-    
+
     String nom;
     double degreAlcool;
     String brasserie;
     boolean ouverte;
-    
-    void lireEtiquette(){
-        System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+
+    void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
     }
-    
-    
+
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    boolean Décapsuler() {
+        if (ouverte == true) {
+            System.out.println("La biere "+nom+" est deja ouverte!");
+            return false;
+        } else {
+            ouverte=true;
+            System.out.println("Biere "+nom+" decapsulee ! Cheers!");
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres) \nOuverte ? "; 
+        if (ouverte == true ){
+            chaine_a_retourner += "oui" ;
+        } else{
+            chaine_a_retourner += "non" ;
+        }
+        return chaine_a_retourner ;
+        }
+
 }
+
