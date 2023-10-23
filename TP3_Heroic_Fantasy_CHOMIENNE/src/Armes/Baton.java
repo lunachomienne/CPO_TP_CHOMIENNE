20641ba602d7f746f4f9af9f81eeb92be0e3799f
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-23 13:02:16 +0200

    tp3

---
+++ TP3_Heroic_Fantasy_CHOMIENNE/src/Armes/Baton.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+
+
+/**
+ *
+ * @author lunac
+ */
+
+public class Baton extends Arme {
+    
+    int age; 
+    
+    public Baton(String pseudo, int niv, int nb) {
+        super(pseudo, niv);
+        age=nb;
+       
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Baton{" + "age=" + age +", niveau d'attaque="+ nivAttaque+ ", nom="+nom+ '}';
+    }
+    
+}
