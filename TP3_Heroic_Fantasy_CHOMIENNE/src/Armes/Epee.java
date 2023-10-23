5829e8afeaffa60d46279a75129ed01d3431fba9
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-23 13:01:56 +0200

    tp3

---
+++ TP3_Heroic_Fantasy_CHOMIENNE/src/Armes/Epee.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author lunac
+ */
+public class Epee extends Arme{
+    
+    int finesse;
+    
+    public Epee(String pseudo, int niv, int fin) {
+        super(pseudo, niv);
+        finesse=fin;
+    }
+    
+    @Override
+    public String toString() {
+        return "Epee{" + "finesse=" + finesse +", niveau d'attaque="+ nivAttaque+ ", nom="+nom+ '}';
+}
+    }
