5fe374e2d7ad1ebd9bc0f61ba431fe85e78b20ad
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-24 11:16:48 +0200

    tp3

---
+++ TP3_Heroic_Fantasy_CHOMIENNE/src/tp3_heroic_fantasy_chomienne/etreVivant.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tp3_heroic_fantasy_chomienne;
+
+/**
+ *
+ * @author lunac
+ */
+public interface etreVivant {
+    
+    public void seFatiguer();
+    
+    boolean estVivant();
+    
+    public void estAttaque(int point);
+    
+}
