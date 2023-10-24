d67a286c560b15500785c2ab1b8ec0c509a8a707
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-24 11:17:04 +0200

    tp3

--- TP3_Heroic_Fantasy_CHOMIENNE/src/Personnages/Personnage.java
+++ TP3_Heroic_Fantasy_CHOMIENNE/src/Personnages/Personnage.java
@@ -6,12 +6,13 @@ package Personnages;
 
 import Armes.Arme;
 import java.util.ArrayList;
+import tp3_heroic_fantasy_chomienne.etreVivant;
 
 /**
  *
  * @author lunac
  */
-public abstract class Personnage {
+public abstract class Personnage implements etreVivant {
     
     String nom;
     int nivVie;
@@ -23,12 +24,37 @@ public abstract class Personnage {
         nivVie=niv;
         nbperso+=1;
     }
-     
     
+    public void finalize(){
+        nbperso=nbperso-1;
+    }
      
     @Override
     public String toString () {
         return "Le personnage "+nom+ " a un niveau de vie de "+nivVie;
     }
+
+    @Override
+    public void seFatiguer() {
+        nivVie=nivVie-10;
+    }
+
+    @Override
+    public boolean estVivant() {
+        if (nivVie>0){
+            System.out.println(nom+" est toujours vivant!");
+          return true;  
+        }else{
+            System.out.println(nom +" a perdu le combat... Rejouez en changeant les armes et attributs des personnages !");
+            return false;
+        }
+    }
+    
+    
+    @Override
+    public void estAttaque(int point) {
+        nivVie=nivVie-point;
+    }
+    
     
 }
