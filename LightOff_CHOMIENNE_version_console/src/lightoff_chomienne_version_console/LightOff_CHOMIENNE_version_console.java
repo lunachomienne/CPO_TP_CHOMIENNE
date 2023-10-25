c8d6ab6e69b69635ee4ddfc9132e5d3d07b51a89
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-25 11:15:33 +0200

    petit projet 1

---
+++ LightOff_CHOMIENNE_version_console/src/lightoff_chomienne_version_console/LightOff_CHOMIENNE_version_console.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_chomienne_version_console;
+
+/**
+ *
+ * lunachomienne TDB le 25/10/2023
+ */
+public class LightOff_CHOMIENNE_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
