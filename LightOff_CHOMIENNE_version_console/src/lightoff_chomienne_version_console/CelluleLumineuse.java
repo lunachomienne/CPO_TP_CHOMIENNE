f0cf415a36932baeef33dc8bac77842dd0072cd5
Luna Chomienne <luna.chomienne@epfedu.fr>
2023-10-27 17:55:36 +0200

    Petit Projet 1: le jeu dans sa version intiale est fini et complet.

--- LightOff_CHOMIENNE_version_console/src/lightoff_chomienne_version_console/CelluleLumineuse.java
+++ LightOff_CHOMIENNE_version_console/src/lightoff_chomienne_version_console/CelluleLumineuse.java
@@ -12,10 +12,16 @@ public class CelluleLumineuse {
     
     private boolean etat;
     
+    /**
+     *le constructeur de la classe, initialise l'état de la cellule à "éteint" (ou false).
+     */
     public CelluleLumineuse(){
         etat=false;
     }
     
+    /**
+     *active une cellule, c'est à dire qu'elle va la passer dans l'état opposé à celui qu'elle était avant la fonction.
+     */
     public void activerCellule(){
         if (etat==true){
             etat=false;
@@ -24,23 +30,38 @@ public class CelluleLumineuse {
         }
     }
     
+    /**
+     *cette fonction passe la cellule dans l'état "éteint".
+     */
     public void eteindreCellule(){
         etat=false;
     }
     
+    /**
+     *cette fonction verifie si la cellule est éteinte. 
+     * @return Si elle n'est pas éteinte, elle renvoie false, et true dans le cas contraire(elle est bien éteinte).
+     */
     public boolean estEteint(){
         if (etat==true){
-            return true;
-        }else{
             return false;
+        }else{
+            return true;
         }
     }
     
+    /**
+     *cette fonction permet de recupérer l'état de la cellule.
+     * @return Elle renvoie false si la cellule est éteinte, et true si elle est allumée.
+     */
     public boolean getEtat(){
         return etat;
     }
     
-     @Override
+    /**
+     *cette fonction formate l'état de la cellule. 
+     * @return Si la cellule lumineuse est éteinte, elle renvoie 0, si elle est allumée, elle renvoie X.
+     */
+    @Override
     public String toString () {
         if (getEtat()==true){
             return "X";
